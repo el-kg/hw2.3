0e755c8a332fa4961aa379f0119b14b5d11c823c
el-kg <kirillgalkin8@gmail.com>
2023-07-16 18:26:06 +0700

    initial commit

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends TransportWithEngine{
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public Car(String modelName) {
+        super(modelName,4);
+    }
+
+}
